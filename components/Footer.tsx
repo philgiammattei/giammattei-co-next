ab1286e6ad7aab23f160d583a9d6a84084c7c74e
Phil Giammattei <philgiammattei@gmail.com>
2024-03-27 17:25:59 -0400

    remove twitter

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,9 +2,6 @@ import React from 'react';
 
 export const Footer: React.FC = () => (
   <div className="footer">
-    <a href="https://twitter.com/philgiammattei">
-      <img src="/img/twitter.svg" alt="Twitter" height="30" width="30" />
-    </a>
     <a rel="me" href="https://wears.tigerpajamas.com/@phil">
       <img src="/img/mastodon.svg" alt="Mastodon" height="30" width="30" />
     </a>
